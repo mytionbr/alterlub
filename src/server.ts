21fd06011faf02ab5b6b86ddc382f37065ae7ee6
mytionBr <matheuss.jesus2017@gmail.com>
2021-12-26 11:50:48 -0300

    add prettier

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
-import "./util/module-alias";
-import { Server } from "@overnightjs/core";
-import bodyParser from "body-parser";
-import { ForecastController } from "./controllers/forecast";
-import { Application } from "express";
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+import { Application } from 'express';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init() : void {
-      this.setupExpress();
+  public init(): void {
+    this.setupExpress();
   }
 
   private setupExpress(): void {
-      this.app.use(bodyParser.json());
-      this.setupController();
+    this.app.use(bodyParser.json());
+    this.setupController();
   }
 
   private setupController(): void {
-      const forecastController = new ForecastController();
-      this.addControllers([forecastController]);
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
   }
 
   public getApp(): Application {
-      return this.app;
+    return this.app;
   }
 }
