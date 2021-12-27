39f0c4a976ad9d9beac654683ee31ccbf5b9439e
mytionBr <matheuss.jesus2017@gmail.com>
2021-12-26 22:52:52 -0300

    add request module

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,7 +1,8 @@
 import { ForecastController } from '@src/controllers/forecast';
 import { InternalError } from '@src/util/errors/internal-error';
 import { AxiosError, AxiosStatic } from 'axios';
-import config, { IConfig } from 'config'
+import config, { IConfig } from 'config';
+import * as HTTPUtil from '@src/util/request';
 
 export interface StormGlassPointSource {
   [key: string]: number;
@@ -56,7 +57,7 @@ export class StormGlass {
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassAPISource = 'noaa';
 
-  constructor(protected request: AxiosStatic) {}
+  constructor(protected request = new HTTPUtil.Request()) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     try {
@@ -70,7 +71,7 @@ export class StormGlass {
       );
       return this.normalizeResponse(response.data);
     } catch (err: unknown) {
-        if ((err as AxiosError).response && (err as AxiosError).response?.data) {
+        if (HTTPUtil.Request.isRequestError(err as AxiosError)) {
             throw new StormGlassResponseError(
               `Error: ${JSON.stringify((err as AxiosError).response?.data)} Code: ${
                 (err as AxiosError).response?.status
